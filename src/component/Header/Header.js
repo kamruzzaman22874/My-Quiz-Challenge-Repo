3a6031ecc0b4d54e8857ba899095d6e0296a6717
kamruzzaman22874 <joybd.eee@gmail.com>
2022-10-12 15:17:46 +0600

    Final Check

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className="navbar bg-slate-200 lg:px-12">
       <div className="flex-1">
-        <p className="normal-case  lg:text-2xl font-bold  text-green-500 lg:px-24">CHALLENGING QUIZ</p>
+        <p className="normal-case  lg:text-2xl text-xl font-bold  lg:text-green-500 lg:px-24  text-blue-600">CHALLENGING QUIZ</p>
       </div>
       <div className="flex-none ml-3 lg:px-[210px] lg:text-2xl font-bold ">
        <Link className="mr-4 hover:text-green-500" to='/home'>Home</Link>
