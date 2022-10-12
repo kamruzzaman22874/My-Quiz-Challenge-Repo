e2b4547ffafadd4a2587f35937912ac44c231829
kamruzzaman22874 <joybd.eee@gmail.com>
2022-10-12 14:29:41 +0600

    final check

--- src/component/ErrorElement/ErrorElement.js
+++ src/component/ErrorElement/ErrorElement.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const ErrorElement = () => {
     return (
-        <div className='text-3xl bg-blue-900 lg:m-36 rounded-xl text-black-600 m-5'>
+        <div className='text-2xl bg-blue-900 lg:m-36 rounded-xl text-black-600 m-5'>
             <h2 className='py-4  lg:w-96 mx-auto  lg:px-[20px] px-[40px] mt-4 mb-4 lg:text-2xl text-xl text-white lg:animate-ping'>🚫This page is not found🚫</h2>
             <img className='lg:w-[400px] lg:ml-[270px] mx-auto text-white rounded-lg' src="https://miro.medium.com/max/1400/1*idpUphkU9dL5atdD3KbO3Q.png" alt="" />
             <Link className='lg:px-[350px] px-[75px] text-white ' to='/home'>Back To Home</Link>
